8b2e3f158d9e0e59e57c780700a8b4c26e189beb
DuncantheeDuncan <mthembup69@gmail.com>
2021-12-28 01:31:20 +0200

    moving

--- src/test/java/node/PrintTreeUsingJDBCTests.java
+++ src/test/java/node/PrintTreeUsingJDBCTests.java
@@ -1,34 +1,34 @@
-package node;
-
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import static org.testng.AssertJUnit.assertEquals;
-
-public class PrintTreeUsingJDBCTests {
-
-    Connection connection;
-
-    @BeforeEach
-    public void setUp() throws SQLException, ClassNotFoundException {
-        PrintTreeUsingJDBC jdbcNodes = new PrintTreeUsingJDBC();
-        connection = DriverManager.getConnection("jdbc:h2:./target/jdbc_addingnodes", "sa", "");
-        Statement statement = connection.createStatement();
-        statement.addBatch("delete from NodesDB");
-        statement.executeBatch();
-    }
-
-    @Test
-    public void shouldAdd() throws SQLException, ClassNotFoundException {
-
-        PrintTreeUsingJDBC add = new PrintTreeUsingJDBC();
-//        add.addNodes(2, "phila", 5);
-
-        assertEquals("K", "k");
-    }
-}
+//package node;
+//
+//import org.junit.Test;
+//import org.junit.jupiter.api.BeforeEach;
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+//import java.sql.Statement;
+//
+//import static org.testng.AssertJUnit.assertEquals;
+//
+//public class PrintTreeUsingJDBCTests {
+//
+//    Connection connection;
+//
+//    @BeforeEach
+//    public void setUp() throws SQLException, ClassNotFoundException {
+//        PrintTreeUsingJDBC jdbcNodes = new PrintTreeUsingJDBC();
+//        connection = DriverManager.getConnection("jdbc:h2:./target/jdbc_addingnodes", "sa", "");
+//        Statement statement = connection.createStatement();
+//        statement.addBatch("delete from NodesDB");
+//        statement.executeBatch();
+//    }
+//
+//    @Test
+//    public void shouldAdd() throws SQLException, ClassNotFoundException {
+//
+//        PrintTreeUsingJDBC add = new PrintTreeUsingJDBC();
+////        add.addNodes(2, "phila", 5);
+//
+//        assertEquals("K", "k");
+//    }
+//}
